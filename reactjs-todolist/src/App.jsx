f47f29d38be17a6ed75af4fc3366e8488dee4c24
perogramar <nkakkatu@gmail.com>
2024-10-11 02:23:24 +0530

    aaa

--- reactjs-todolist/src/App.jsx
+++ reactjs-todolist/src/App.jsx
@@ -1,22 +1,25 @@
 import TodoList from "./components/Todolist"
 import TodoInput from "./components/Todoinput"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 function App() {
-  
- 
-
   const [todos,setTodos] = useState([])
   const [todoValue,setTodoValue] = useState('')
 
+  function persistData(newList) {
+    localStorage.setItem('todos', JSON.stringify({ todos: newList }))
+  }
+
   function handleAddTodos(newTodo) {
     const newTodoList =[...todos, newTodo]
+    persistData(newTodoList)
     setTodos(newTodoList)
   }
   function handleDeleteTodo(index){
     const newTodoList = todos.filter((todo, todoIndex) => {
       return todoIndex != index
     })
+    persistData(newTodoList)
     setTodos(newTodoList)
   }
 
@@ -26,6 +29,22 @@ function App() {
     handleDeleteTodo(index)
     
   } 
+  useEffect(() => {
+    if (!localStorage) {
+      return
+    }
+
+    let localTodos = localStorage.getItem('todos')
+    if (!localTodos) {
+      return
+    }
+
+    console.log(localTodos)
+    localTodos = JSON.parse(localTodos).todos
+    setTodos(localTodos)
+
+  }, [])
+
 
 
 
