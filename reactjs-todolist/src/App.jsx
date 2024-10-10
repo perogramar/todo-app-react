0a512f4ac0601c1ebc588f875d201ee27da57297
perogramar <nkakkatu@gmail.com>
2024-10-11 02:11:11 +0530

    first commit

---
+++ reactjs-todolist/src/App.jsx
@@ -0,0 +1,40 @@
+import TodoList from "./components/Todolist"
+import TodoInput from "./components/Todoinput"
+import { useState } from "react"
+
+function App() {
+  
+ 
+
+  const [todos,setTodos] = useState([])
+  const [todoValue,setTodoValue] = useState('')
+
+  function handleAddTodos(newTodo) {
+    const newTodoList =[...todos, newTodo]
+    setTodos(newTodoList)
+  }
+  function handleDeleteTodo(index){
+    const newTodoList = todos.filter((todo, todoIndex) => {
+      return todoIndex != index
+    })
+    setTodos(newTodoList)
+  }
+
+  function handleEditTodo(index){
+    const valueToBeEdited = todos[index]
+    setTodoValue(valueToBeEdited)
+    handleDeleteTodo(index)
+    
+  } 
+
+
+
+  return (
+    <>
+      <TodoInput todoValue={todoValue} setTodoValue={setTodoValue} handleAddTodos={handleAddTodos}/>
+      <TodoList handleEditTodo={handleEditTodo} handleDeleteTodo={handleDeleteTodo} todos={todos}/>
+    </>
+  )
+}
+
+export default App
